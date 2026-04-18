b3847f81957fc4629175f074e81329aba985d349
Alexander <aleksandrzubarev6@gmail.com>
2026-04-18 03:30:47 +0300

    test: add tests

---
+++ lab3/Presentation/src/test/java/AccountControllerTests.java
@@ -0,0 +1,170 @@
+import Accounts.AccountDto;
+import Runners.App;
+import Services.AccountService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = App.class)
+public class AccountControllerTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AccountService accountService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void testCreateAccount() throws Exception {
+        UUID userId = UUID.randomUUID();
+
+        AccountDto testAccount = new AccountDto(UUID.randomUUID(),
+                BigDecimal.ZERO,
+                userId);
+
+        Mockito.when(accountService.createAccount(any())).thenReturn(testAccount);
+
+        String requestJson = """
+                {
+                    "userId": "%s"
+                }
+                """.formatted(userId);
+
+        String resultJson = mockMvc
+                .perform(post("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+                {
+                    "userId": "%s",
+                    "balance": 0
+                }
+                """.formatted(userId), resultJson, JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    public void testPutMoney() throws Exception {
+        UUID accountId = UUID.randomUUID();
+
+        AccountDto testAccount = new AccountDto(accountId ,
+                new BigDecimal(200),
+                UUID.randomUUID());
+
+        Mockito.when(accountService.putMoney(any()))
+                .thenReturn(testAccount);
+
+        String requestJson = """
+                {
+                    "accountId": "%s",
+                    "money": 200
+                }
+                """.formatted(accountId);
+
+        String resultJson = mockMvc
+                .perform(patch("/api/accounts/put")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+                {
+                    "balance": 200
+                }
+                """, resultJson, JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        AccountDto testAccount1 = new AccountDto(
+                UUID.randomUUID(),
+                new BigDecimal(239),
+                UUID.randomUUID()
+        );
+
+        AccountDto testAccount2 = new AccountDto(
+                UUID.randomUUID(),
+                new BigDecimal(30),
+                UUID.randomUUID()
+        );
+
+        Mockito.when(accountService.findAll())
+                .thenReturn(List.of(testAccount1, testAccount2));
+
+        String resultJson = mockMvc
+                .perform(get("/api/accounts/all"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+                [
+                    { "balance": 239 },
+                    { "balance": 30 }
+                ]
+                """, resultJson, JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    public void testFindByUserId() throws Exception {
+        UUID userId = UUID.randomUUID();
+
+        AccountDto testAccount = new AccountDto(
+                UUID.randomUUID(),
+                new BigDecimal(67),
+                userId
+        );
+
+        Mockito.when(accountService.findByUserId(any()))
+                .thenReturn(List.of(testAccount));
+
+        String resultJson = mockMvc
+                .perform(get("/api/accounts/users/{userId}", userId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+            [
+                {
+                    "userId": "%s",
+                    "balance": 67
+                }
+            ]
+            """.formatted(userId), resultJson, JSONCompareMode.LENIENT);
+    }
+}
---
+++ lab3/Presentation/src/test/java/OperationControllerTests.java
@@ -0,0 +1,111 @@
+import Operations.OperationDto;
+import Operations.OperationType;
+import Runners.App;
+import Services.OperationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = App.class)
+public class OperationControllerTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private OperationService operationService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void testGetHistory() throws Exception {
+        UUID accountId = UUID.randomUUID();
+
+        OperationDto testOperation1 = new OperationDto(
+                UUID.randomUUID(),
+                OperationType.PUT,
+                LocalDateTime.now(),
+                new BigDecimal(100),
+                accountId
+        );
+
+        OperationDto testOperation2 = new OperationDto(
+                UUID.randomUUID(),
+                OperationType.WITHDRAW,
+                LocalDateTime.now(),
+                new BigDecimal(20),
+                accountId
+        );
+
+        Mockito.when(operationService.getHistory(any()))
+                .thenReturn(List.of(testOperation1, testOperation2));
+
+        String resultJson = mockMvc
+                .perform(get("/api/operation/{accountId}", accountId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+                [
+                    { "type": "PUT", "money": 100 },
+                    { "type": "WITHDRAW", "money": 20 }
+                ]
+                """, resultJson, JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    public void testFindByTypeAndAccount() throws Exception {
+        UUID accountId = UUID.randomUUID();
+
+        OperationDto testOperation = new OperationDto(
+                UUID.randomUUID(),
+                OperationType.PUT,
+                LocalDateTime.now(),
+                new BigDecimal(100),
+                accountId
+        );
+
+        Mockito.when(operationService.findByTypeAndAccount(any()))
+                .thenReturn(List.of(testOperation));
+
+        String resultJson = mockMvc
+                .perform(get("/api/operation/filters")
+                        .param("type", "PUT")
+                        .param("accountId", accountId.toString()))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals("""
+                [
+                    { "type": "PUT", "money": 100 }
+                ]
+                """, resultJson, JSONCompareMode.LENIENT);
+    }
+}
